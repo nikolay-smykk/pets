d384d7ea4ebe260021a6cba63cbbfa4f3aa3641b
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-07-24 23:06:05 +0300

    init

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,32 @@
+import './style.css'
+
 export const NotFound = () => {
     return (
         <>
-            <h1>Not Found</h1>
+            <div className="site-logo">minimalizm-studio</div>
+            <div className="navbar">
+                <div className="site-info">Платье для вас</div>
+                <div className="site-menu">
+                    <div className="menu-item">Projects</div>
+                    <div className="menu-item">About</div>
+                    <div className="menu-item">Contact</div>
+                </div>
+            </div>
+
+            <div className="header-container">
+                <div className="header">
+                    <span>4</span>
+                    <span>0</span>
+                    <span>4</span>
+                </div>
+            </div>
+
+            <div className="footer">
+                <span>
+                    Seems you are lost, come back <u>Home</u>
+                </span>
+                <div className="footer-wrapper"></div>
+            </div>
         </>
     )
 }
