338a041034a8420150b7c9140805965b96144e4e
Nikolay Smyk <nikolaysmyk3@gmail.com>
2022-08-02 11:57:37 +0300

    init

--- sypress/cypress.config.js
+++ sypress/cypress.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://alidi.ru/",
-    ignoreTestFIles: "**/example/*",
+    baseUrl: 'http://alidi.ru',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
